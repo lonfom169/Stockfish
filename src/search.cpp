54e8bb7fb1e79893e813233ce2a24adcc9eb3b8a
lonfom169 <lomalfredo8@gmail.com>
2020-11-22 03:21:13 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1223,7 +1223,7 @@ moves_loop: // When in check, search starts from here
           else
           {
               // Increase reduction for captures/promotions if late move and at low depth
-              if (depth < 8 && moveCount > 2)
+              if (depth < 12 && moveCount > 2)
                   r++;
 
               // Unless giving check, this capture is likely bad
