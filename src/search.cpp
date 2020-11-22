56f0c66b700a118b771fe9f638426471d1cfb822
lonfom169 <lomalfredo8@gmail.com>
2020-11-22 02:24:35 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -600,7 +600,7 @@ namespace {
 
     TTEntry* tte;
     Key posKey;
-    Move ttMove, move, excludedMove, bestMove;
+    Move ttMove, move, excludedMove, bestMove, prevBestMove = MOVE_NONE;
     Depth extension, newDepth;
     Value bestValue, value, ttValue, eval, maxValue, probCutBeta;
     bool formerPv, givesCheck, improving, didLMR, priorCapture;
@@ -1170,7 +1170,7 @@ moves_loop: // When in check, search starts from here
               r -= 2;
 
           // Increase reduction at root and non-PV nodes when the best move does not change frequently
-          if ((rootNode || !PvNode) && depth > 10 && thisThread->bestMoveChanges <= 2)
+          if ((rootNode || !PvNode) && depth > 10 && thisThread->bestMoveChanges <= 2 && thisThread->stableCount < 5)
               r++;
 
           if (moveCountPruning && !formerPv)
@@ -1318,6 +1318,8 @@ moves_loop: // When in check, search starts from here
               rm.score = -VALUE_INFINITE;
       }
 
+      bestMove = prevBestMove;
+
       if (value > bestValue)
       {
           bestValue = value;
@@ -1339,6 +1341,11 @@ moves_loop: // When in check, search starts from here
               }
           }
       }
+
+      if (bestMove == prevBestMove)
+          ++thisThread->stableCount;
+      else
+          thisThread->stableCount = 0;
 
       if (move != bestMove)
       {
