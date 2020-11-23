82fc09f1a0f58f70af3e896ab1a4a5a181d94a72
lonfom169 <lomalfredo8@gmail.com>
2020-11-23 13:51:32 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1173,6 +1173,9 @@ moves_loop: // When in check, search starts from here
           if ((rootNode || !PvNode) && depth > 10 && thisThread->bestMoveChanges <= 2)
               r++;
 
+          if (abs(ss->staticEval - (ss-2)->staticEval) > 50)
+              r--;
+
           if (moveCountPruning && !formerPv)
               r++;
 
