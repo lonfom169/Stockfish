8df7659b1694277be8f7dc631ce6cee53c977d76
lonfom169 <lomalfredo8@gmail.com>
2020-12-16 22:50:29 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1227,7 +1227,8 @@ moves_loop: // When in check, search starts from here
                              + (*contHist[0])[movedPiece][to_sq(move)]
                              + (*contHist[1])[movedPiece][to_sq(move)]
                              + (*contHist[3])[movedPiece][to_sq(move)]
-                             - 5287;
+                             - 2243
+                             - moveCount * moveCountPruning * 192;
 
               // Decrease/increase reduction by comparing opponent's stat score (~10 Elo)
               if (ss->statScore >= -105 && (ss-1)->statScore < -103)
