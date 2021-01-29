0304d8829f1e5595538986df72842749fcfabb75
lonfom169 <lomalfredo8@gmail.com>
2021-01-29 14:52:32 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1176,6 +1176,9 @@ moves_loop: // When in check, search starts from here
           if (ss->ttPv)
               r -= 2;
 
+          if (rootNode && depth > 7 && thisThread->bmThreshold >= 0)
+              r++;
+
           // Increase reduction at root and non-PV nodes when the best move does not change frequently
           if ((rootNode || !PvNode) && thisThread->rootDepth > 10 && thisThread->bestMoveChanges <= 2)
               r++;
@@ -1332,6 +1335,7 @@ moves_loop: // When in check, search starts from here
           if (value > alpha)
           {
               bestMove = move;
+              thisThread->bmThreshold = 2;
 
               if (PvNode && !rootNode) // Update pv even in fail-high case
                   update_pv(ss->pv, move, (ss+1)->pv);
@@ -1356,6 +1360,8 @@ moves_loop: // When in check, search starts from here
           else if (!captureOrPromotion && quietCount < 64)
               quietsSearched[quietCount++] = move;
       }
+      else
+          thisThread->bmThreshold--;
     }
 
     // The following condition would detect a stop only after move loop has been
