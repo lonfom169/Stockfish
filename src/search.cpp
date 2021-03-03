b5994cab5d4fbeb080d42c18914457e7f21e8f27
lonfom169 <lomalfredo8@gmail.com>
2021-03-02 21:15:21 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1206,6 +1206,9 @@ moves_loop: // When in check, search starts from here
           if (ss->ttPv && !likelyFailLow)
               r -= 2;
 
+          if (ss->ply == 1 && depth < 9 && thisThread->bestMoveChanges > 8)
+              r--;
+
           // Increase reduction at root and non-PV nodes when the best move does not change frequently
           if ((rootNode || !PvNode) && thisThread->rootDepth > 10 && thisThread->bestMoveChanges <= 2)
               r++;
