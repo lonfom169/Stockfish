398cf32d5a6b98172e8f253d470cc406ee8293a7
lonfom169 <lomalfredo8@gmail.com>
2021-03-24 20:56:06 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1791,13 +1791,8 @@ moves_loop: // When in check, search starts from here
   void update_continuation_histories(Stack* ss, Piece pc, Square to, int bonus) {
 
     for (int i : {1, 2, 4, 6})
-    {
-        // Only update first 2 continuation histories if we are in check
-        if (ss->inCheck && i > 2)
-            break;
         if (is_ok((ss-i)->currentMove))
             (*(ss-i)->continuationHistory)[pc][to] << bonus;
-    }
   }
 
 
