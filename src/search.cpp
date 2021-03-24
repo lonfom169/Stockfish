f5337015e34aa3bef28be21b7130348e3641b178
lonfom169 <lomalfredo8@gmail.com>
2021-03-24 14:36:11 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1248,7 +1248,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~10 Elo)
               if (cutNode)
-                  r += 2;
+                  r += 2 - (depth > 12 && !(thisThread->nodes & 0x9));
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
