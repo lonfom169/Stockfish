1dd475013b810d2931f6aec2bb21a55a3e59190b
lonfom169 <lomalfredo8@gmail.com>
2021-03-25 20:18:12 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1077,7 +1077,7 @@ moves_loop: // When in check, search starts from here
                   continue;
 
               // SEE based pruning
-              if (!pos.see_ge(move, Value(-218) * depth)) // (~25 Elo)
+              if (!pos.see_ge(move, Value(-218) * depth) && !ttMove) // (~25 Elo)
                   continue;
           }
           else
