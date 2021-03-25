a768bf4f491a32c41e02d90dcc2cf443f4f0c4c8
lonfom169 <lomalfredo8@gmail.com>
2021-03-24 21:11:00 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1773,7 +1773,7 @@ moves_loop: // When in check, search starts from here
     // main killer move in previous ply when it gets refuted.
     if (   ((ss-1)->moveCount == 1 + (ss-1)->ttHit || ((ss-1)->currentMove == (ss-1)->killers[0]))
         && !pos.captured_piece())
-            update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, -bonus1);
+            update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, -bonus1 / 2);
 
     // Decrease stats for all non-best capture moves
     for (int i = 0; i < captureCount; ++i)
