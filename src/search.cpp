95482069719e575437423f3bed86c7b7d6bf1bd7
lonfom169 <lomalfredo8@gmail.com>
2021-04-22 15:06:46 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1203,6 +1203,11 @@ moves_loop: // When in check, search starts from here
               && !likelyFailLow)
               r -= 2;
 
+          if (   ttMove
+              && abs(pos.non_pawn_material(us) - pos.non_pawn_material(~us)) > KnightValueMg
+              && ttValue > 120)
+              r--;
+
           // Increase reduction at root and non-PV nodes when the best move does not change frequently
           if (   (rootNode || !PvNode)
               && thisThread->rootDepth > 10
