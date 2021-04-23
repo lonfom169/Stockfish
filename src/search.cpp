da8a1ab54f894b9d711cbbd82571218ae38bf698
lonfom169 <lomalfredo8@gmail.com>
2021-04-23 00:16:22 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1064,7 +1064,7 @@ moves_loop: // When in check, search starts from here
           moveCountPruning = moveCount >= futility_move_count(improving, depth);
 
           // Reduced depth of the next LMR search
-          int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount), 0);
+          int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount), 1);
 
           if (   captureOrPromotion
               || givesCheck)
