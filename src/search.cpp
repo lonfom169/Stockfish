95c49fbd6b37f2aff908cf1982582fd898be32c9
lonfom169 <lomalfredo8@gmail.com>
2021-04-27 16:14:20 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1231,6 +1231,10 @@ moves_loop: // When in check, search starts from here
               if (   !givesCheck
                   && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 210 * depth <= alpha)
                   r++;
+
+              if (   givesCheck
+                  && ss->staticEval + PieceValue[EG][pos.captured_piece()] - 140 * depth >= alpha)
+                  r--;
           }
           else
           {
