0ef3923440dda2a5343072d3c7d999cf15fabfef
lonfom169 <lomalfredo8@gmail.com>
2021-04-27 21:11:13 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1130,7 +1130,7 @@ moves_loop: // When in check, search starts from here
           {
               extension = 1;
               singularQuietLMR = !ttCapture;
-              if (!PvNode && value < singularBeta - 140)
+              if (!PvNode && value < singularBeta - 140 + givesCheck * 44)
                   extension = 2;
           }
 
