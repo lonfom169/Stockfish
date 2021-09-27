0e364f8371c7a0c17c97ef5bef4d3ac043288ead
lonfom169 <lomalfredo8@gmail.com>
2021-09-27 18:11:05 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -880,14 +880,14 @@ namespace {
     // If we have a good enough capture and a reduced search returns a value
     // much above beta, we can (almost) safely prune the previous move.
     if (   !PvNode
-        &&  depth > 4
+        &&  depth > 3
         &&  abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
         // if value from transposition table is lower than probCutBeta, don't attempt probCut
-        // there and in further interactions with transposition table cutoff depth is set to depth - 3
-        // because probCut search has depth set to depth - 4 but we also do a move before it
-        // so effective depth is equal to depth - 3
+        // there and in further interactions with transposition table cutoff depth is set to depth - 2
+        // because probCut search has depth set to depth - 3 but we also do a move before it
+        // so effective depth is equal to depth - 2
         && !(   ss->ttHit
-             && tte->depth() >= depth - 3
+             && tte->depth() >= depth - 2
              && ttValue != VALUE_NONE
              && ttValue < probCutBeta))
     {
@@ -903,7 +903,7 @@ namespace {
             if (move != excludedMove && pos.legal(move))
             {
                 assert(pos.capture_or_promotion(move));
-                assert(depth >= 5);
+                assert(depth >= 4);
 
                 captureOrPromotion = true;
                 probCutCount++;
@@ -921,7 +921,7 @@ namespace {
 
                 // If the qsearch held, perform the regular search
                 if (value >= probCutBeta)
-                    value = -search<NonPV>(pos, ss+1, -probCutBeta, -probCutBeta+1, depth - 4, !cutNode);
+                    value = -search<NonPV>(pos, ss+1, -probCutBeta, -probCutBeta+1, depth - 3, !cutNode);
 
                 pos.undo_move(move);
 
@@ -929,11 +929,11 @@ namespace {
                 {
                     // if transposition table doesn't have equal or more deep info write probCut data into it
                     if ( !(ss->ttHit
-                       && tte->depth() >= depth - 3
+                       && tte->depth() >= depth - 2
                        && ttValue != VALUE_NONE))
                         tte->save(posKey, value_to_tt(value, ss->ply), ttPv,
                             BOUND_LOWER,
-                            depth - 3, move, ss->staticEval);
+                            depth - 2, move, ss->staticEval);
                     return value;
                 }
             }
