58ac12ba8f6414f1a4e500e3dbdbfbeb418339d4
lonfom169 <lomalfredo8@gmail.com>
2021-09-28 15:51:05 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1234,11 +1234,11 @@ moves_loop: // When in check, search starts here
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions if
           // newDepth got its own extension before).
-          int deeper =   r >= -1               ? 0
-                       : noLMRExtension        ? 0
-                       : moveCount <= 5        ? 1
-                       : (depth > 6 && PvNode) ? 1
-                       :                         0;
+          int deeper =   r >= -1                 ? 0
+                       : noLMRExtension          ? 0
+                       : moveCount <= 5          ? 1
+                       : (depth > 6 && ss->ttPv) ? 1
+                       :                           0;
 
           Depth d = std::clamp(newDepth - r, 1, newDepth + deeper);
 
