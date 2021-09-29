be79e8fce91a8ed018f2108c2ddaa948c12732cf
lonfom169 <lomalfredo8@gmail.com>
2021-09-29 20:27:32 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1152,6 +1152,7 @@ moves_loop: // When in check, search starts here
 
       // Quiet ttMove extensions
       else if (   PvNode
+               && depth > 6
                && move == ttMove
                && move == ss->killers[0]
                && (*contHist[0])[movedPiece][to_sq(move)] >= 10000)
