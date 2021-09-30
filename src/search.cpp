a1780adb859f0148c9949d93c2ebbbd2d9aeb665
lonfom169 <lomalfredo8@gmail.com>
2021-09-30 19:37:21 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -587,7 +587,7 @@ namespace {
     Move ttMove, move, excludedMove, bestMove;
     Depth extension, newDepth;
     Value bestValue, value, ttValue, eval, maxValue, probCutBeta;
-    bool givesCheck, improving, didLMR, priorCapture;
+    bool givesCheck, improving, didLMR, didSE, priorCapture;
     bool captureOrPromotion, doFullDepthSearch, moveCountPruning,
          ttCapture, singularQuietLMR, noLMRExtension;
     Piece movedPiece;
@@ -600,6 +600,7 @@ namespace {
     moveCount          = captureCount = quietCount = ss->moveCount = 0;
     bestValue          = -VALUE_INFINITE;
     maxValue           = VALUE_INFINITE;
+    didSE              = ss->didSE;
 
     // Check for the available remaining time
     if (thisThread == Threads.main())
@@ -1063,7 +1064,7 @@ moves_loop: // When in check, search starts here
               if (lmrDepth < 5
                   && (*contHist[0])[movedPiece][to_sq(move)]
                   + (*contHist[1])[movedPiece][to_sq(move)]
-                  + (*contHist[3])[movedPiece][to_sq(move)] < -3000 * depth + 3000)
+                  + (*contHist[3])[movedPiece][to_sq(move)] < (-3000 - 400 * didSE) * depth + 3000)
                   continue;
 
               // Futility pruning: parent node (~5 Elo)
@@ -1098,7 +1099,9 @@ moves_loop: // When in check, search starts here
           Depth singularDepth = (depth - 1) / 2;
 
           ss->excludedMove = move;
+          ss->didSE = true;
           value = search<NonPV>(pos, ss, singularBeta - 1, singularBeta, singularDepth, cutNode);
+          ss->didSE = false;
           ss->excludedMove = MOVE_NONE;
 
           if (value < singularBeta)
@@ -1130,7 +1133,9 @@ moves_loop: // When in check, search starts here
           else if (ttValue >= beta)
           {
               ss->excludedMove = move;
+              ss->didSE = true;
               value = search<NonPV>(pos, ss, beta - 1, beta, (depth + 3) / 2, cutNode);
+              ss->didSE = false;
               ss->excludedMove = MOVE_NONE;
 
               if (value >= beta)
