cf9f609ce7451173259790e2ae24d48e540a3613
lonfom169 <lomalfredo8@gmail.com>
2021-10-03 12:39:58 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1107,7 +1107,8 @@ moves_loop: // When in check, search starts here
               singularQuietLMR = !ttCapture;
 
               // Avoid search explosion by limiting the number of double extensions
-              if (   !PvNode
+              if (   (!PvNode
+                  || (PvNode && moveCount == 1))
                   && value < singularBeta - 75
                   && ss->doubleExtensions <= 6)
               {
