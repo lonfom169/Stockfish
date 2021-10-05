90e88f4a1fc9be912d3f71f38c2931768ffa16a0
lonfom169 <lomalfredo8@gmail.com>
2021-10-04 23:29:20 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1188,7 +1188,7 @@ moves_loop: // When in check, search starts here
           Depth r = reduction(improving, depth, moveCount, rangeReduction > 2);
 
           if (PvNode)
-              r--;
+              r -= 1 + (moveCount == 2);
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage.is_greater(537, 1024))
