e1755a84d55f8588bcb8622abd26d37abe48398c
lonfom169 <lomalfredo8@gmail.com>
2021-10-20 12:33:55 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -935,12 +935,12 @@ namespace {
     // Step 10. If the position is not in TT, decrease depth by 2 or 1 depending on node type
     if (   PvNode
         && depth >= 6
-        && !ttMove)
+        && moveCount > 2)
         depth -= 2;
 
     if (   cutNode
         && depth >= 9
-        && !ttMove)
+        && moveCount > 2)
         depth--;
 
 moves_loop: // When in check, search starts here
