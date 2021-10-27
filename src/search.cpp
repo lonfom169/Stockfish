8895f1c5349628de3555b68e9aea968eac9a0f8f
lonfom169 <lomalfredo8@gmail.com>
2021-10-27 00:20:53 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1181,6 +1181,11 @@ moves_loop: // When in check, search starts here
               && thisThread->bestMoveChanges <= 2)
               r++;
 
+          if (   rootNode
+              && thisThread->bestMoveChanges <= 2
+              && moveCount > 20)
+              r++;
+
           // Decrease reduction if opponent's move count is high (~1 Elo)
           if ((ss-1)->moveCount > 13)
               r--;
