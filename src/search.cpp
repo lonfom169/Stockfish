cc07895a2c36fd887ffcac4284bd46b850d7b676
lonfom169 <lomalfredo8@gmail.com>
2021-11-15 08:55:53 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1260,7 +1260,7 @@ moves_loop: // When in check, search starts here
           (ss+1)->pv[0] = MOVE_NONE;
 
           value = -search<PV>(pos, ss+1, -beta, -alpha,
-                              std::min(maxNextDepth, newDepth), false);
+                              std::min(maxNextDepth, newDepth - (newDepth > 1 && moveCount > 2)), false);
       }
 
       // Step 18. Undo move
