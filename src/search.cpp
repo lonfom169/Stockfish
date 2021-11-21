43a4bd0a3a7fec3002aaafdcad5786af98f9a7a4
lonfom169 <lomalfredo8@gmail.com>
2021-11-21 12:42:07 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1104,6 +1104,7 @@ moves_loop: // When in check, search starts here
               // Avoid search explosion by limiting the number of double extensions
               if (   !PvNode
                   && value < singularBeta - 75
+                  && abs(ttValue) < 1560
                   && ss->doubleExtensions <= 6)
                   extension = 2;
           }
