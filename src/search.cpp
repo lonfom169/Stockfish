48c322e95bbc9d3f99ec2022877f702e3cb5f57b
lonfom169 <lomalfredo8@gmail.com>
2021-11-27 12:08:06 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1037,7 +1037,7 @@ moves_loop: // When in check, search starts here
           moveCountPruning = moveCount >= futility_move_count(improving, depth);
 
           // Reduced depth of the next LMR search
-          int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount, rangeReduction > 2), 0);
+          int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount, rangeReduction > 3), 0);
 
           if (   captureOrPromotion
               || givesCheck)
@@ -1171,7 +1171,7 @@ moves_loop: // When in check, search starts here
               || !captureOrPromotion
               || (cutNode && (ss-1)->moveCount > 1)))
       {
-          Depth r = reduction(improving, depth, moveCount, rangeReduction > 2);
+          Depth r = reduction(improving, depth, moveCount, rangeReduction > 3);
 
           // Decrease reduction at some PvNodes (~2 Elo)
           if (   PvNode
