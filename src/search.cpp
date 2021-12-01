83e999a6feef87adaf44e4a63cfaa26d17d99fb3
lonfom169 <lomalfredo8@gmail.com>
2021-11-30 22:42:06 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1226,7 +1226,7 @@ moves_loop: // When in check, search starts here
 
           Depth d = std::clamp(newDepth - r, 1, newDepth + deeper);
 
-          value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
+          value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, !PvNode);
 
           // Range reductions (~3 Elo)
           if (ss->staticEval - value < 30 && depth > 7)
