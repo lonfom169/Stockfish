a181595e356191429280b14a739ba2c2fd291e15
lonfom169 <lomalfredo8@gmail.com>
2021-12-09 19:53:38 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -592,7 +592,7 @@ namespace {
     Value bestValue, value, ttValue, eval, maxValue, probCutBeta;
     bool givesCheck, improving, didLMR, priorCapture;
     bool captureOrPromotion, doFullDepthSearch, moveCountPruning,
-         ttCapture, singularQuietLMR;
+         ttCapture, singularQuietLMR, singularPV;
     Piece movedPiece;
     int moveCount, captureCount, quietCount, bestMoveCount, improvement;
 
@@ -983,7 +983,7 @@ moves_loop: // When in check, search starts here
                                       ss->ply);
 
     value = bestValue;
-    singularQuietLMR = moveCountPruning = false;
+    singularQuietLMR = singularPV = moveCountPruning = false;
 
     // Indicate PvNodes that will probably fail low if the node was searched
     // at a depth equal or greater than the current depth, and the result of this search was a fail low.
@@ -1108,6 +1108,7 @@ moves_loop: // When in check, search starts here
           {
               extension = 1;
               singularQuietLMR = !ttCapture;
+              singularPV = PvNode && value < singularBeta - 66;
 
               // Avoid search explosion by limiting the number of double extensions
               if (   !PvNode
@@ -1229,7 +1230,7 @@ moves_loop: // When in check, search starts here
                        : cutNode && moveCount <= 7 ? 1
                        :                             0;
 
-          Depth d = std::clamp(newDepth - r, 1, newDepth + deeper);
+          Depth d = std::clamp(newDepth - r * !(singularPV && moveCount <= 14 && r), 1, newDepth + deeper);
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
