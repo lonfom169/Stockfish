ebcc0a6e7b9ffdb62bea6d07401e5ef517534a5c
lonfom169 <lomalfredo8@gmail.com>
2021-12-13 09:04:58 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -819,7 +819,7 @@ namespace {
     // Step 7. Futility pruning: child node (~50 Elo).
     // The depth condition is important for mate finding.
     if (   !ss->ttPv
-        &&  depth < 9
+        &&  depth < 9 - 3 * excludedMove
         &&  eval - futility_margin(depth, improving) >= beta
         &&  eval < 15000) // 50% larger than VALUE_KNOWN_WIN, but smaller than TB wins.
         return eval;
