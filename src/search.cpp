7ad824c5c39fb3db99c8afae381c0981875d7701
lonfom169 <lomalfredo8@gmail.com>
2021-12-23 13:31:11 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1114,7 +1114,7 @@ moves_loop: // When in check, search starts here
 
           // If the eval of ttMove is greater than beta, we reduce it (negative extension)
           else if (ttValue >= beta)
-              extension = -2;
+              extension = -2 + (value < beta);
       }
 
       // Check extensions (~1 Elo)
