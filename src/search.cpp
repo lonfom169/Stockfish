858201d7221702998a6e171bba77c56a822892fa
lonfom169 <lomalfredo8@gmail.com>
2021-12-28 17:06:59 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -858,7 +858,10 @@ namespace {
             thisThread->nmpMinPly = 0;
 
             if (v >= beta)
+            {
+                update_continuation_histories(ss, pos.moved_piece(ss->currentMove), to_sq(ss->currentMove), stat_bonus(depth-R-1));
                 return nullValue;
+            }
         }
     }
 
