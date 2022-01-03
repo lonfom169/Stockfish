7a57485edb491e3b9d77b3b5aaf915e1318b3d61
lonfom169 <lomalfredo8@gmail.com>
2022-01-03 09:00:47 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1238,6 +1238,9 @@ moves_loop: // When in check, search starts here
           (ss+1)->pv = pv;
           (ss+1)->pv[0] = MOVE_NONE;
 
+          if (PvNode && extension)
+              alpha -= 2;
+
           value = -search<PV>(pos, ss+1, -beta, -alpha,
                               std::min(maxNextDepth, newDepth), false);
       }
