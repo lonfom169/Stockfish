08b801d8db96d7839bc2200e20d762e7f50c444f
lonfom169 <lomalfredo8@gmail.com>
2022-07-27 12:34:04 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -404,7 +404,7 @@ void Thread::search() {
               // re-search, otherwise exit the loop.
               if (bestValue <= alpha)
               {
-                  beta = (alpha + beta) / 2;
+                  beta = beta - (beta - alpha) / 3;
                   alpha = std::max(bestValue - delta, -VALUE_INFINITE);
 
                   failedHighCnt = 0;
