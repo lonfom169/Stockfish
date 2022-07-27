5b04d25c8ad3530256264318df98fafd9d15cf75
lonfom169 <lomalfredo8@gmail.com>
2022-07-27 12:34:02 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1147,9 +1147,8 @@ moves_loop: // When in check, search starts here
 
           // Decrease reduction if position is or has been on the PV
           // and node is not likely to fail low. (~3 Elo)
-          if (   ss->ttPv
-              && !likelyFailLow)
-              r -= 2;
+          if (ss->ttPv)
+              r -= 2 - likelyFailLow;
 
           // Decrease reduction if opponent's move count is high (~1 Elo)
           if ((ss-1)->moveCount > 7)
