21f71c826317add9071fd1d7002f33d3050ac095
lonfom169 <lomalfredo8@gmail.com>
2022-07-27 12:37:55 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -905,7 +905,7 @@ namespace {
     // Use qsearch if depth is equal or below zero (~4 Elo)
     if (    PvNode
         && !ttMove)
-        depth -= 3;
+        depth -= 3 - pos.capture(ss->currentMove);
 
     if (depth <= 0)
         return qsearch<PV>(pos, ss, alpha, beta);
