3ad49df437363fa105b6f44201f1f0e2fedc3c6d
lonfom169 <lomalfredo8@gmail.com>
2022-08-01 19:36:23 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -62,8 +62,8 @@ namespace {
   enum NodeType { NonPV, PV, Root };
 
   // Futility margin
-  Value futility_margin(Depth d, bool improving) {
-    return Value(168 * (d - improving));
+  Value futility_margin(Depth d, bool improving, bool excludedMove) {
+    return Value((168 + 32 * excludedMove) * (d - improving));
   }
 
   // Reductions lookup table, initialized at startup
@@ -794,7 +794,7 @@ namespace {
     // The depth condition is important for mate finding.
     if (   !ss->ttPv
         &&  depth < 8
-        &&  eval - futility_margin(depth, improving) - (ss-1)->statScore / 256 >= beta
+        &&  eval - futility_margin(depth, improving, excludedMove) - (ss-1)->statScore / 256 >= beta
         &&  eval >= beta
         &&  eval < 26305) // larger than VALUE_KNOWN_WIN, but smaller than TB wins.
         return eval;
