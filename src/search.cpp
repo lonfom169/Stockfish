825f13be9b680e9cb03639f69362464bf1cde8c8
lonfom169 <lomalfredo8@gmail.com>
2022-08-01 20:19:04 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1093,7 +1093,7 @@ moves_loop: // When in check, search starts here
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension)
               else if (ttValue >= beta)
-                  extension = -2;
+                  extension = -3;
 
               // If the eval of ttMove is less than alpha and value, we reduce it (negative extension)
               else if (ttValue <= alpha && ttValue <= value)
