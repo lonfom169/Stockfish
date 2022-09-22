ac6bd097ae780f9f1dae158ee3d7ea3ccb9e98fb
lonfom169 <lomalfredo8@gmail.com>
2022-09-22 16:22:05 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1297,6 +1297,9 @@ moves_loop: // When in check, search starts here
       else
          ss->cutoffCnt = 0;
 
+      if (value > Value(1860))
+          moveCount++;
+
 
       // If the move is worse than some previously searched move, remember it to update its stats later
       if (move != bestMove)
