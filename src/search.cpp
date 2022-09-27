855642ef3e6bfe842dfe54aefe137b61f845d415
lonfom169 <lomalfredo8@gmail.com>
2022-09-26 23:34:03 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1147,6 +1147,9 @@ moves_loop: // When in check, search starts here
           if ((ss-1)->moveCount > 7)
               r--;
 
+          if (to_sq(move) == prevSq)
+              r++;
+
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode)
               r += 2;
