b7f25e9edeb461bf14db80b1ac2bb2606e8990e0
lonfom169 <lomalfredo8@gmail.com>
2022-09-27 06:58:47 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1054,7 +1054,7 @@ moves_loop: // When in check, search starts here
               &&  move == ttMove
               && !excludedMove // Avoid recursive singular search
            /* &&  ttValue != VALUE_NONE Already implicit in the next condition */
-              &&  abs(ttValue) < VALUE_KNOWN_WIN
+              &&  abs(ttValue) < 8000
               && (tte->bound() & BOUND_LOWER)
               &&  tte->depth() >= depth - 3)
           {
