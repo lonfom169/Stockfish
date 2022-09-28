53ff20de34431a611ba8925a2f54459b379f290e
lonfom169 <lomalfredo8@gmail.com>
2022-09-27 22:22:23 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1149,7 +1149,7 @@ moves_loop: // When in check, search starts here
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode)
-              r += 2;
+              r += 2 + (depth <= 3);
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
