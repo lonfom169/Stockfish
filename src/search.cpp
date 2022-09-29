5127710f5c849769783ab123607c2b598ca9a7ff
lonfom169 <lomalfredo8@gmail.com>
2022-09-29 19:13:55 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1160,7 +1160,7 @@ moves_loop: // When in check, search starts here
               r -= 1 + 11 / (3 + depth);
 
           // Decrease reduction if ttMove has been singularly extended (~1 Elo)
-          if (singularQuietLMR)
+          if (!PvNode && singularQuietLMR)
               r--;
 
           // Increase reduction if next ply has a lot of fail high else reset count to 0
