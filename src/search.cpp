a046c53c42d286694930ec564efae653fc783a57
lonfom169 <lomalfredo8@gmail.com>
2022-10-07 15:17:32 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -502,6 +502,9 @@ void Thread::search() {
 
       mainThread->iterValue[iterIdx] = bestValue;
       iterIdx = (iterIdx + 1) & 3;
+
+      if (rootDepth < 8)
+          rootDepth++;
   }
 
   if (!mainThread)
