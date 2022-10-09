1bfd0436a35125ae243941d5463daf8959a92736
lonfom169 <lomalfredo8@gmail.com>
2022-10-08 21:06:58 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1514,7 +1514,7 @@ moves_loop: // When in check, search starts here
           &&  type_of(move) != PROMOTION)
       {
 
-          if (moveCount > 2)
+          if (moveCount > 2 + PvNode)
               continue;
 
           futilityValue = futilityBase + PieceValue[EG][pos.piece_on(to_sq(move))];
