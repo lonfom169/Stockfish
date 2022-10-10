21eb96353715d6e6eacf7d8cb211415ddfb85c9e
lonfom169 <lomalfredo8@gmail.com>
2022-10-10 11:48:58 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1171,6 +1171,9 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
+          if (depth == 2)
+              r++;
+
           ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
