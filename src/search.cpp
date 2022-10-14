9ca8abf6498f7362afbb280e0a16667faed82773
lonfom169 <lomalfredo8@gmail.com>
2022-10-14 18:03:03 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1178,7 +1178,7 @@ moves_loop: // When in check, search starts here
                          - 4433;
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-          r -= ss->statScore / 13628;
+          r -= (ss->statScore - (ss-1)->statScore) / 20000;
 
           // In general we want to cap the LMR depth search at newDepth, but when
           // reduction is negative, we allow this move a limited search extension
