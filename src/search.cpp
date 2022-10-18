44bc5a79f4a313b5a5b459caa0e2ea1b1c9634c8
lonfom169 <lomalfredo8@gmail.com>
2022-10-18 00:04:45 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1548,8 +1548,8 @@ moves_loop: // When in check, search starts here
       // Continuation history based pruning (~2 Elo)
       if (   !capture
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY
-          && (*contHist[0])[pos.moved_piece(move)][to_sq(move)] < 0
-          && (*contHist[1])[pos.moved_piece(move)][to_sq(move)] < 0)
+          && (*contHist[0])[pos.moved_piece(move)][to_sq(move)] < -71
+          && (*contHist[1])[pos.moved_piece(move)][to_sq(move)] < -71)
           continue;
 
       // movecount pruning for quiet check evasions
