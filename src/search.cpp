6c98d45978c144156009beac9879d0af7b83b647
lonfom169 <lomalfredo8@gmail.com>
2022-11-04 17:33:53 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1190,10 +1190,11 @@ moves_loop: // When in check, search starts here
           if (value > alpha && d < newDepth)
           {
               const bool doDeeperSearch = value > (alpha + 64 + 11 * (newDepth - d));
+              Value lmrValue = value;
               value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth + doDeeperSearch, !cutNode);
 
-              int bonus = value > alpha ?  stat_bonus(newDepth)
-                                        : -stat_bonus(newDepth);
+              int bonus = value > alpha ?  stat_bonus(newDepth) + (value - lmrValue) * 8
+                                        : -stat_bonus(newDepth) - (lmrValue - value) * 4;
 
               if (capture)
                   bonus /= 6;
