2f8408626d8bc0ec23b33596e11039864715f1ec
lonfom169 <lomalfredo8@gmail.com>
2022-11-06 01:12:40 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1208,6 +1208,9 @@ moves_loop: // When in check, search starts here
               value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth, !cutNode);
       }
 
+      if (PvNode && moveCount > 39 && value < bestValue + 18)
+          newDepth--;
+
       // For PV nodes only, do a full PV search on the first move or after a fail
       // high (in the latter case search only if value < beta), otherwise let the
       // parent node fail low with value <= alpha and try another move.
