be4a56a826a8ac4bbf24ee72620d3e7f5700efc6
lonfom169 <lomalfredo8@gmail.com>
2022-11-07 16:03:34 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1084,7 +1084,10 @@ moves_loop: // When in check, search starts here
 
               // If the eval of ttMove is less than alpha and value, we reduce it (negative extension)
               else if (ttValue <= alpha && ttValue <= value)
+              {
                   extension = -1;
+                  depth--;
+              }
           }
 
           // Check extensions (~1 Elo)
