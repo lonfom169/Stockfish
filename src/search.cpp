0356b03a0ced385f1103a147de212cae9b34cd79
lonfom169 <lomalfredo8@gmail.com>
2022-11-07 14:11:07 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1181,6 +1181,9 @@ moves_loop: // When in check, search starts here
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
+          if (value < bestValue + 24)
+              newDepth--;
+
           // Do full depth search when reduced LMR search fails high
           if (value > alpha && d < newDepth)
           {
