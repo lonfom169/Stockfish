9f959a9722120ea175c375b6790f6b8c8034ef5a
lonfom169 <lomalfredo8@gmail.com>
2022-11-10 01:25:44 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1161,6 +1161,9 @@ moves_loop: // When in check, search starts here
               && (mp.threatenedPieces & from_sq(move)))
               r--;
 
+          if (mp.threatenedPieces & to_sq(move))
+              r++;
+
           // Increase reduction if next ply has a lot of fail high
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
