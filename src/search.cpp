f0062264f30f1e46077a34d9297de28b9c01dbcc
lonfom169 <50217346+lonfom169@users.noreply.github.com>
2022-11-16 21:47:20 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -983,6 +983,7 @@ moves_loop: // When in check, search starts here
 
       // Step 14. Pruning at shallow depth (~98 Elo). Depth conditions are important for mate finding.
       if (  !rootNode
+          && (!excludedMove || moveCount > 2)
           && pos.non_pawn_material(us)
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
