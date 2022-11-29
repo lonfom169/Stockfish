995b15eb41bec70d47ec631096ca0530506f40b1
lonfom169 <lomalfredo8@gmail.com>
2022-11-28 23:10:34 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -75,8 +75,8 @@ namespace {
   }
 
   constexpr int futility_move_count(bool improving, Depth depth) {
-    return improving ? (3 + depth * depth)
-                     : (3 + depth * depth) / 2;
+    return (improving ? (3 + depth * depth)
+                      : (3 + depth * depth) / 2) + (depth < 4 ? -1 : depth < 7 ? 0 : depth < 15 ? 1 : 2);
   }
 
   // History and stats update bonus, based on depth
