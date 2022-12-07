9f2e401ba7d3d2f737a8fe4877dc433941739999
Alfredo Menezes <lomalfredo8@gmail.com>
2022-12-07 01:02:39 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1134,7 +1134,7 @@ moves_loop: // When in check, search starts here
           // and node is not likely to fail low. (~3 Elo)
           if (   ss->ttPv
               && !likelyFailLow)
-              r -= 2;
+              r -= 2 - (depth - tte->depth() > 2);
 
           // Decrease reduction if opponent's move count is high (~1 Elo)
           if ((ss-1)->moveCount > 7)
