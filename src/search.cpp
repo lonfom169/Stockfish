8e42281a05817a9ecd06716164e8b1700d8572b2
Alfredo Menezes <lomalfredo8@gmail.com>
2022-12-17 13:07:07 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -75,8 +75,8 @@ namespace {
   }
 
   constexpr int futility_move_count(bool improving, Depth depth) {
-    return improving ? (3 + depth * depth)
-                     : (3 + depth * depth) / 2;
+    return std::max(improving ? (3 + depth * depth)
+                              : (3 + depth * depth) / 2, 4);
   }
 
   // History and stats update bonus, based on depth
