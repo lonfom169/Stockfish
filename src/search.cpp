b826fd440d4ae05dd46f6f3676a862be07d7271e
Alfredo Menezes <lomalfredo8@gmail.com>
2022-12-22 22:55:25 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1138,7 +1138,7 @@ moves_loop: // When in check, search starts here
 
       // Increase reduction if ttMove is a capture (~3 Elo)
       if (ttCapture)
-          r++;
+          r += 1 + capture;
 
       // Decrease reduction for PvNodes based on depth
       if (PvNode)
