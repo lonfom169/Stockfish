c269bcedcf12fddd3ffdfb8e550a998748f40723
Alfredo Menezes <lomalfredo8@gmail.com>
2023-01-12 05:27:59 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1144,7 +1144,7 @@ moves_loop: // When in check, search starts here
           r++;
 
       // Decrease reduction for PvNodes based on depth
-      if (PvNode)
+      if (PvNode && !rootNode)
           r -= 1 + 11 / (3 + depth);
 
       // Decrease reduction if ttMove has been singularly extended (~1 Elo)
