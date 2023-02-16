489ec44e0b6c63049a68e25046454021771221c4
Alfredo Menezes <lomalfredo8@gmail.com>
2023-02-16 00:15:57 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1208,7 +1208,7 @@ moves_loop: // When in check, search starts here
               // was good enough search deeper, if it was bad enough search shallower
               const bool doDeeperSearch = value > (alpha + 66 + 11 * (newDepth - d));
               const bool doEvenDeeperSearch = value > alpha + 582 && ss->doubleExtensions <= 5;
-              const bool doShallowerSearch = value < bestValue + newDepth;
+              const bool doShallowerSearch = value < bestValue + newDepth && !excludedMove;
 
               ss->doubleExtensions = ss->doubleExtensions + doEvenDeeperSearch;
 
