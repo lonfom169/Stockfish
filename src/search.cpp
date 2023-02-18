ee5b1e87faae32caf6f6abc97157775f21e3c7c4
Alfredo Menezes <lomalfredo8@gmail.com>
2023-02-18 01:20:10 -0300

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -746,6 +746,7 @@ namespace {
 
         // ttValue can be used as a better position evaluation (~7 Elo)
         if (    ttValue != VALUE_NONE
+            &&  tte->depth() > 4
             && (tte->bound() & (ttValue > eval ? BOUND_LOWER : BOUND_UPPER)))
             eval = ttValue;
     }
