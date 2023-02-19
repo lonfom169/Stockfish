615a269d62d951afb02bf68fe3a9eba2d18b98c4
Alfredo Menezes <lomalfredo8@gmail.com>
2023-02-19 19:48:58 -0300

    bench_3126028

--- src/search.cpp
+++ src/search.cpp
@@ -630,7 +630,7 @@ namespace {
         ss->ttPv = PvNode || (ss->ttHit && tte->is_pv());
 
     // At non-PV nodes we check for an early TT cutoff
-    if (  !PvNode
+    if (  (!PvNode || (!tte->is_pv() && tte->bound() == BOUND_UPPER))
         && ss->ttHit
         && !excludedMove
         && tte->depth() > depth - (tte->bound() == BOUND_EXACT)
