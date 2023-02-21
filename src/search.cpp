0b1db0e11c8ebc22b04a038b291694a484536fb5
Alfredo Menezes <lomalfredo8@gmail.com>
2023-02-21 18:55:49 -0300

    Bench: 4181434

--- src/search.cpp
+++ src/search.cpp
@@ -1316,13 +1316,10 @@ moves_loop: // When in check, search starts here
                   alpha = value;
 
                   // Reduce other moves if we have found at least one score improvement
-                  if (   depth > 1
-                      && depth < 6
+                  if (   depth < 6
                       && beta  <  VALUE_KNOWN_WIN
                       && alpha > -VALUE_KNOWN_WIN)
                       depth -= 1;
-
-                  assert(depth > 0);
               }
               else
               {
