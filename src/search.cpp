6e62d5465da022c565ee922a944b4c68ec61e057
Alfredo Menezes <lomalfredo8@gmail.com>
2023-02-21 09:48:15 -0300

    tweak Bench: 4122121

--- src/search.cpp
+++ src/search.cpp
@@ -1173,7 +1173,7 @@ moves_loop: // When in check, search starts here
       // Decrease reduction if move is a killer and we have a good history
       if (move == ss->killers[0]
           && (*contHist[0])[movedPiece][to_sq(move)] >= 3600)
-          r--;
+          r -= 1 + PvNode;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
