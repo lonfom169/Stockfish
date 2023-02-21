4e9af641935326c6eb35cff8add840e3e4bbc9cc
Alfredo Menezes <lomalfredo8@gmail.com>
2023-02-21 12:20:16 -0300

    Bench: 4041177

--- src/search.cpp
+++ src/search.cpp
@@ -1013,7 +1013,7 @@ moves_loop: // When in check, search starts here
                   continue;
 
               // SEE based pruning (~11 Elo)
-              if (!pos.see_ge(move, Value(-220) * depth))
+              if (!pos.see_ge(move, Value(-400) * lmrDepth))
                   continue;
           }
           else
