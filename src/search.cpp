62b1089db40d05aa56aaf54fbd5f5114d7a9047a
Alfredo Menezes <lomalfredo8@gmail.com>
2023-02-28 12:56:21 -0300

    Bench: 4567861

--- src/search.cpp
+++ src/search.cpp
@@ -752,7 +752,7 @@ namespace {
 
         // ttValue can be used as a better position evaluation (~7 Elo)
         if (    ttValue != VALUE_NONE
-            && (tte->bound() & (ttValue > eval ? BOUND_LOWER : BOUND_UPPER)))
+            && ((tte->bound() & (ttValue > eval ? BOUND_LOWER : BOUND_UPPER)) || tte->is_pv()))
             eval = ttValue;
     }
     else
