615f16e8ca7a2f800c9ebb7be6760bd2aaf3bb83
Alfredo Menezes <lomalfredo8@gmail.com>
2023-03-04 19:32:05 -0300

    Bench: 4643183

--- src/search.cpp
+++ src/search.cpp
@@ -1156,6 +1156,11 @@ moves_loop: // When in check, search starts here
       if (cutNode)
           r += 2;
 
+      if (   !ttMove
+          && !ss->inCheck
+          && eval >= beta)
+          r++;
+
       // Increase reduction if ttMove is a capture (~3 Elo)
       if (ttCapture)
           r++;
