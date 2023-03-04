ad4c7c8540127b1b865623b763f43faf092d2672
Alfredo Menezes <lomalfredo8@gmail.com>
2023-03-04 19:32:29 -0300

    Bench: 5670668

--- src/search.cpp
+++ src/search.cpp
@@ -122,8 +122,9 @@ namespace {
   void update_pv(Move* pv, Move move, const Move* childPv);
   void update_continuation_histories(Stack* ss, Piece pc, Square to, int bonus);
   void update_quiet_stats(const Position& pos, Stack* ss, Move move, int bonus);
-  void update_all_stats(const Position& pos, Stack* ss, Move bestMove, Value bestValue, Value beta, Square prevSq,
-                        Move* quietsSearched, int quietCount, Move* capturesSearched, int captureCount, Depth depth);
+  void update_all_stats(const Position& pos, Stack* ss, Move bestMove, Move prevBestMove, Value bestValue, Value beta,
+                        Square prevSq, Move* quietsSearched, int quietCount, Move* capturesSearched, int captureCount,
+                        Depth depth);
 
   // perft() is our utility to verify move generation. All the leaf nodes up
   // to the given depth are generated and counted, and the sum is returned.
@@ -551,7 +552,7 @@ namespace {
 
     TTEntry* tte;
     Key posKey;
-    Move ttMove, move, excludedMove, bestMove;
+    Move ttMove, move, excludedMove, bestMove, prevBestMove;
     Depth extension, newDepth;
     Value bestValue, value, ttValue, eval, maxValue, probCutBeta;
     bool givesCheck, improving, priorCapture, singularQuietLMR;
@@ -602,7 +603,7 @@ namespace {
     assert(0 <= ss->ply && ss->ply < MAX_PLY);
 
     (ss+1)->ttPv         = false;
-    (ss+1)->excludedMove = bestMove = MOVE_NONE;
+    (ss+1)->excludedMove = bestMove = prevBestMove = MOVE_NONE;
     (ss+2)->killers[0]   = (ss+2)->killers[1] = MOVE_NONE;
     (ss+2)->cutoffCnt    = 0;
     ss->doubleExtensions = (ss-1)->doubleExtensions;
@@ -1317,6 +1318,7 @@ moves_loop: // When in check, search starts here
           if (value > alpha)
           {
               bestMove = move;
+              prevBestMove = bestMove;
 
               if (PvNode && !rootNode) // Update pv even in fail-high case
                   update_pv(ss->pv, move, (ss+1)->pv);
@@ -1377,7 +1379,7 @@ moves_loop: // When in check, search starts here
 
     // If there is a move which produces search value greater than alpha we update stats of searched moves
     else if (bestMove)
-        update_all_stats(pos, ss, bestMove, bestValue, beta, prevSq,
+        update_all_stats(pos, ss, bestMove, prevBestMove, bestValue, beta, prevSq,
                          quietsSearched, quietCount, capturesSearched, captureCount, depth);
 
     // Bonus for prior countermove that caused the fail low
@@ -1705,8 +1707,9 @@ moves_loop: // When in check, search starts here
 
   // update_all_stats() updates stats at the end of search() when a bestMove is found
 
-  void update_all_stats(const Position& pos, Stack* ss, Move bestMove, Value bestValue, Value beta, Square prevSq,
-                        Move* quietsSearched, int quietCount, Move* capturesSearched, int captureCount, Depth depth) {
+  void update_all_stats(const Position& pos, Stack* ss, Move bestMove, Move prevBestMove, Value bestValue, Value beta,
+                        Square prevSq, Move* quietsSearched, int quietCount, Move* capturesSearched,
+                        int captureCount, Depth depth) {
 
     Color us = pos.side_to_move();
     Thread* thisThread = pos.this_thread();
@@ -1733,6 +1736,17 @@ moves_loop: // When in check, search starts here
     else
         // Increase stats for the best move in case it was a capture move
         captureHistory[moved_piece][to_sq(bestMove)][captured] << bonus1;
+
+    if (prevBestMove)
+    {
+        if (!pos.capture(prevBestMove))
+        {
+            thisThread->mainHistory[us][from_to(prevBestMove)] << stat_bonus(depth);
+            update_continuation_histories(ss, pos.moved_piece(prevBestMove), to_sq(prevBestMove), stat_bonus(depth));
+        }
+        else
+            captureHistory[pos.moved_piece(prevBestMove)][to_sq(prevBestMove)][type_of(pos.piece_on(to_sq(prevBestMove)))] << bonus1;
+    }
 
     // Extra penalty for a quiet early move that was not a TT move or
     // main killer move in previous ply when it gets refuted.
