03ecc00e81567ffbf08c8827afa8873252b00872
Alfredo Menezes <lomalfredo8@gmail.com>
2023-03-08 16:46:17 -0300

    Bench: 4871951

--- src/search.cpp
+++ src/search.cpp
@@ -1242,6 +1242,8 @@ moves_loop: // When in check, search starts here
           if (!ttMove && cutNode)
               r += 2;
 
+          r -= extension;
+
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 4), !cutNode);
       }
 
