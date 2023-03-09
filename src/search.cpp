db39de3fcb0aff1e8d340bb1709c9f1b01c946e3
Alfredo Menezes <lomalfredo8@gmail.com>
2023-03-09 12:44:14 -0300

    Bench: 4573208

--- src/search.cpp
+++ src/search.cpp
@@ -854,7 +854,7 @@ namespace {
     // Step 10. ProbCut (~10 Elo)
     // If we have a good enough capture (or queen promotion) and a reduced search returns a value
     // much above beta, we can (almost) safely prune the previous move.
-    if (   !PvNode
+    if (   !ss->ttPv
         &&  depth > 4
         &&  abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
         // if value from transposition table is lower than probCutBeta, don't attempt probCut
