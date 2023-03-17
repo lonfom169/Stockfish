174e2655ec4fbc07bd24ca0a2c22c31f94a66631
Alfredo Menezes <lomalfredo8@gmail.com>
2023-03-17 13:18:07 -0300

    Bench: 4494293

--- src/search.cpp
+++ src/search.cpp
@@ -1164,7 +1164,7 @@ moves_loop: // When in check, search starts here
 
       // Decrease reduction for PvNodes based on depth
       if (PvNode)
-          r -= 1 + 12 / (3 + depth);
+          r--;
 
       // Decrease reduction if ttMove has been singularly extended (~1 Elo)
       if (singularQuietLMR)
