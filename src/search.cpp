82c6a47fed818b57da309e82847c0f570a6748be
Alfredo Menezes <lomalfredo8@gmail.com>
2023-03-17 10:54:55 -0300

    Bench: 4825779

--- src/search.cpp
+++ src/search.cpp
@@ -1102,7 +1102,10 @@ moves_loop: // When in check, search starts here
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension)
               else if (ttValue >= beta)
-                  extension = -2 - !PvNode;
+                  if (PvNode)
+                      extension = -2;
+                  else
+                      return ttValue;
 
               // If the eval of ttMove is less than value, we reduce it (negative extension)
               else if (ttValue <= value)
