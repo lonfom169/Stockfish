133970231b77ac5ac25445f9f951b8e7ac63a386
Alfredo Menezes <lomalfredo8@gmail.com>
2023-03-17 10:54:33 -0300

    Bench: 4543191

--- src/search.cpp
+++ src/search.cpp
@@ -1124,7 +1124,7 @@ moves_loop: // When in check, search starts here
                    && move == ttMove
                    && move == ss->killers[0]
                    && (*contHist[0])[movedPiece][to_sq(move)] >= 5705)
-              extension = 1;
+              extension = 1 + (depth < tte->depth() - 2);
       }
 
       // Add extension to new depth
