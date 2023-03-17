3e46f6a89d07cf8cf8d34bfde59e0d454b6b131d
Alfredo Menezes <lomalfredo8@gmail.com>
2023-03-17 14:57:09 -0300

    Bench: 5031901

--- src/search.cpp
+++ src/search.cpp
@@ -1084,7 +1084,8 @@ moves_loop: // When in check, search starts here
 
                   // Avoid search explosion by limiting the number of double extensions
                   if (  !PvNode
-                      && value < singularBeta - 25
+                      && (value < singularBeta - 25
+                      || depth < tte->depth() - 4)
                       && ss->doubleExtensions <= 10)
                   {
                       extension = 2;
