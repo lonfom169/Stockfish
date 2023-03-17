2f181fdb27273b8e54dadd6ed7894465924ed590
Alfredo Menezes <lomalfredo8@gmail.com>
2023-03-17 10:55:49 -0300

    Bench: 4426880

--- src/search.cpp
+++ src/search.cpp
@@ -1243,6 +1243,9 @@ moves_loop: // When in check, search starts here
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 4), !cutNode);
       }
 
+      if (!rootNode && PvNode && extension && value <= alpha - 600)
+          newDepth--;
+
       // For PV nodes only, do a full PV search on the first move or after a fail
       // high (in the latter case search only if value < beta), otherwise let the
       // parent node fail low with value <= alpha and try another move.
