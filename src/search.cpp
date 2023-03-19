0ad740935eed5b10f56d535e71cf7aae4c6ec969
Alfredo Menezes <lomalfredo8@gmail.com>
2023-03-19 09:47:59 -0300

    Bench: 4368051

--- src/search.cpp
+++ src/search.cpp
@@ -1155,10 +1155,6 @@ moves_loop: // When in check, search starts here
       if ((ss-1)->moveCount > 7)
           r--;
 
-      // Increase reduction for cut nodes (~3 Elo)
-      if (cutNode)
-          r += 2;
-
       // Increase reduction if ttMove is a capture (~3 Elo)
       if (ttCapture)
           r++;
@@ -1193,6 +1189,9 @@ moves_loop: // When in check, search starts here
 
       // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
       r -= ss->statScore / (11791 + 3992 * (depth > 6 && depth < 19));
+
+      if (cutNode)
+          r += r > 4 ? 1 : 2;
 
       // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
       // We use various heuristics for the sons of a node after the first son has
