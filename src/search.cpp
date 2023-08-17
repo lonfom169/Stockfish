a0845d595f3f96807f6b95880640c49e507f87c9
lonfom169 <50217346+lonfom169@users.noreply.github.com>
2023-08-16 21:35:28 -0300

    Bench: 1493890

--- src/search.cpp
+++ src/search.cpp
@@ -1040,7 +1040,7 @@ moves_loop: // When in check, search starts here
               &&  move == ttMove
               && !excludedMove // Avoid recursive singular search
            /* &&  ttValue != VALUE_NONE Already implicit in the next condition */
-              &&  abs(ttValue) < VALUE_KNOWN_WIN
+              &&  abs(ttValue) < 5000
               && (tte->bound() & BOUND_LOWER)
               &&  tte->depth() >= depth - 3)
           {
