0501de55ebd1f903ac8e235db5e996d1ac59171d
lonfom169 <50217346+lonfom169@users.noreply.github.com>
2023-08-24 22:25:45 -0300

    Bench: 1617676

--- src/search.cpp
+++ src/search.cpp
@@ -1138,7 +1138,7 @@ moves_loop: // When in check, search starts here
 
       // Decrease reduction for PvNodes (~2 Elo)
       if (PvNode)
-          r--;
+          r -= 1 + (tte->depth() > depth + 3);
 
       // Decrease reduction if ttMove has been singularly extended (~1 Elo)
       if (singularQuietLMR)
