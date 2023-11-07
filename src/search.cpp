d643b7b507e62413818abbfd3cd3602f13011fbb
lonfom169 <50217346+lonfom169@users.noreply.github.com>
2023-11-07 12:07:43 -0300

    Bench: 1474448

--- src/search.cpp
+++ src/search.cpp
@@ -885,8 +885,8 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
                 if (value >= probCutBeta)
                 {
                     // Save ProbCut data into transposition table
-                    tte->save(posKey, value_to_tt(value, ss->ply), ss->ttPv, BOUND_LOWER, depth - 3,
-                              move, ss->staticEval);
+                    tte->save(posKey, value_to_tt(value - (probCutBeta - beta), ss->ply), ss->ttPv,
+                              BOUND_LOWER, depth - 3, move, ss->staticEval);
                     return value - (probCutBeta - beta);
                 }
             }
