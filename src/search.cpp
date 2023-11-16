766aa908591b0fae7adb842efc9d2896a19e7f0e
lonfom169 <50217346+lonfom169@users.noreply.github.com>
2023-11-16 13:42:58 -0300

    Bench: 1383822

--- src/search.cpp
+++ src/search.cpp
@@ -1081,7 +1081,7 @@ moves_loop:  // When in check, search starts here
 
                 // If the ttMove is assumed to fail high over currnet beta (~7 Elo)
                 else if (ttValue >= beta)
-                    extension = -2 - !PvNode;
+                    extension = -1 - (depth < 9) - !PvNode;
 
                 // If we are on a cutNode but the ttMove is not assumed to fail high over current beta (~1 Elo)
                 else if (cutNode)
