52d20f50a45880308dfba2366506f0e48c349d58
lonfom169 <50217346+lonfom169@users.noreply.github.com>
2023-11-19 01:48:49 -0300

    Bench: 1448892

--- src/search.cpp
+++ src/search.cpp
@@ -1205,6 +1205,7 @@ moves_loop:  // When in check, search starts here
                                            : 0;
 
                 update_continuation_histories(ss, movedPiece, to_sq(move), bonus);
+                thisThread->pawnHistory[pawn_structure(pos)][movedPiece][to_sq(move)] << bonus / 4;
             }
         }
 
