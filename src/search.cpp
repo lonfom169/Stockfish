b080201e1639041a399600819929dd49169821a2
lonfom169 <50217346+lonfom169@users.noreply.github.com>
2023-11-21 14:47:02 -0300

    Bench: 1395393

--- src/search.cpp
+++ src/search.cpp
@@ -1057,7 +1057,8 @@ moves_loop:  // When in check, search starts here
                     singularQuietLMR = !ttCapture;
 
                     // Avoid search explosion by limiting the number of double extensions
-                    if (!PvNode && value < singularBeta - 18 && ss->doubleExtensions <= 11)
+                    if (!PvNode && value < singularBeta - 30 + 12 * cutNode
+                        && ss->doubleExtensions <= 11)
                     {
                         extension = 2;
                         depth += depth < 15;
