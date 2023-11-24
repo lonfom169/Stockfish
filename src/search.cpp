33bd2ce99b1bcd6bfe0ddd5d5a5c41010c82e0cf
lonfom169 <50217346+lonfom169@users.noreply.github.com>
2023-11-24 01:05:48 -0300

    Bench: 1407866

--- src/search.cpp
+++ src/search.cpp
@@ -1297,7 +1297,7 @@ moves_loop:  // When in check, search starts here
 
                 if (value >= beta)
                 {
-                    ss->cutoffCnt += 1 + !ttMove;
+                    ss->cutoffCnt += 1 + (!PvNode && !ttMove);
                     assert(value >= beta);  // Fail high
                     break;
                 }
