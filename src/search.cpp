af7790d55a6d46fabb5634bb7eab838c89f68945
lonfom169 <50217346+lonfom169@users.noreply.github.com>
2023-11-28 13:24:52 -0300

    Bench: 1503870

--- src/search.cpp
+++ src/search.cpp
@@ -1124,7 +1124,7 @@ moves_loop:  // When in check, search starts here
 
         // Decrease reduction if position is or has been on the PV (~4 Elo)
         if (ss->ttPv && !likelyFailLow)
-            r -= cutNode && tte->depth() >= depth ? 3 : 2;
+            r -= cutNode ? (tte->depth() >= depth ? 3 : tte->depth() < depth - 2 ? 1 : 2) : 2;
 
         // Decrease reduction if opponent's move count is high (~1 Elo)
         if ((ss - 1)->moveCount > 7)
