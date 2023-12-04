781865d8920b17cd5f174a160818a8da9ffb4297
lonfom169 <50217346+lonfom169@users.noreply.github.com>
2023-12-04 12:14:14 -0300

    Bench: 1219693

--- src/search.cpp
+++ src/search.cpp
@@ -1011,7 +1011,7 @@ moves_loop:  // When in check, search starts here
 
                 // Futility pruning: parent node (~13 Elo)
                 if (!ss->inCheck && lmrDepth < 13
-                    && ss->staticEval + (bestValue < ss->staticEval - 62 ? 123 : 77)
+                    && ss->staticEval + (ss->ttPv || bestValue < ss->staticEval - 62 ? 123 : 77)
                            + 127 * lmrDepth
                          <= alpha)
                     continue;
