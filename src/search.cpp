4e7926698292c5bbcfa010d8024bef3991daedce
lonfom169 <50217346+lonfom169@users.noreply.github.com>
2023-12-09 20:06:31 -0300

    Bench: 1288776

--- src/search.cpp
+++ src/search.cpp
@@ -1507,7 +1507,7 @@ Value qsearch(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth) {
         moveCount++;
 
         // Step 6. Pruning
-        if (bestValue > VALUE_TB_LOSS_IN_MAX_PLY && pos.non_pawn_material(us))
+        if (bestValue > VALUE_TB_LOSS_IN_MAX_PLY && pos.non_pawn_material(us) && move != ttMove)
         {
             // Futility pruning and moveCount pruning (~10 Elo)
             if (!givesCheck && to_sq(move) != prevSq && futilityBase > VALUE_TB_LOSS_IN_MAX_PLY
