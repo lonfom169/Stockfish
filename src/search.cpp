d1bb5a5d0414963aeb94cc4bc44d03aa55e81279
lonfom169 <50217346+lonfom169@users.noreply.github.com>
2023-12-12 01:47:53 -0300

    Bench: 1351135

--- src/search.cpp
+++ src/search.cpp
@@ -1403,7 +1403,7 @@ Value qsearch(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth) {
     Move     ttMove, move, bestMove;
     Depth    ttDepth;
     Value    bestValue, value, ttValue, futilityValue, futilityBase;
-    bool     pvHit, givesCheck, capture;
+    bool     pvHit, givesCheck, capture, ttCapture;
     int      moveCount;
     Color    us = pos.side_to_move();
 
@@ -1433,6 +1433,7 @@ Value qsearch(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth) {
     tte     = TT.probe(posKey, ss->ttHit);
     ttValue = ss->ttHit ? value_from_tt(tte->value(), ss->ply, pos.rule50_count()) : VALUE_NONE;
     ttMove  = ss->ttHit ? tte->move() : MOVE_NONE;
+    ttCapture = ttMove && pos.capture_stage(ttMove);
     pvHit   = ss->ttHit && tte->is_pv();
 
     // At non-PV nodes we check for an early TT cutoff
@@ -1595,6 +1596,9 @@ Value qsearch(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth) {
                     break;  // Fail high
             }
         }
+
+        if (ttCapture)
+            break;
     }
 
     // Step 9. Check for mate
