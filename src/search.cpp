ded588e8dc89d8cc2de706bc04a564a319b0fe83
lonfom169 <50217346+lonfom169@users.noreply.github.com>
2024-02-04 21:54:09 -0300

    Bench: 1228841

--- src/search.cpp
+++ src/search.cpp
@@ -1106,7 +1106,7 @@ moves_loop:  // When in check, search starts here
 
         // Decrease reduction if position is or has been on the PV (~5 Elo)
         if (ss->ttPv)
-            r -= 1 + (ttValue > alpha) + (ttValue > beta && tte->depth() >= depth);
+            r -= 1 + (ttValue > beta || tte->depth() >= depth);
 
         // Decrease reduction if opponent's move count is high (~1 Elo)
         if ((ss - 1)->moveCount > 7)
